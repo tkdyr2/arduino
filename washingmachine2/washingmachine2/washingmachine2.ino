3e6cd2a5379ddc245b2ba510312dc81774395777
park hoon <anrmrdowk@gmail.com>
2019-06-11 20:48:29 +0900

    add library and fix wm1,2-+

--- washingmachine2/washingmachine2/washingmachine2.ino
+++ washingmachine2/washingmachine2/washingmachine2.ino
@@ -6,18 +6,18 @@
 #define DOUT  A1 //엠프 데이터 아웃 핀 넘버 선언
 #define CLK  A0  //엠프 클락 핀 넘버 
 HX711 scale(DOUT, CLK); //엠프 핀 선언 
-int buttonApin = 5; //세탁기1 버튼1
-int buttonBpin = 6; //세탁기1 버튼2
-int buttonCpin = 7; //세탁기1 버튼3
+int buttonApin = 5; //세탁기2 버튼1
+int buttonBpin = 6; //세탁기2 버튼2
+int buttonCpin = 7; //세탁기2 버튼3
 int red = 8;  //빨간색 LED
 int white = 9; //하얀색 LED
 int blue = 10; //파란색 LED
 
-int state = 1; //세탁기1 상태 0..고장 1..사용가능 2..사용중
-int rg = 0; //세탁기1 예약 0..예약안됨 1..예약 중
+int state = 1; //세탁기2 상태 0..고장 1..사용가능 2..사용중
+int rg = 0; //세탁기2 예약 0..예약안됨 1..예약 중
 Timer t;
 int pin = 13; //타이머 설정 핀
-int lefttime = 1;  //세탁기1 남은빨래시간 가정
+int lefttime = 1;  //세탁기2 남은빨래시간 가정
 int mode = 0; //세탁기 빨래 모드 0..저 1..중 2..고(가정)
 
 void setup() {
@@ -100,19 +100,19 @@ void loop() {
     if(digitalRead(buttonCpin) == LOW)
    {
      if(state == 0){  //세탁기 고장 상태 
-        Serial.println("세탁기1 고장");
+        Serial.println("세탁기2 고장");
      }
     if(state == 1){
       state = 2; //사용 시작
       if(state == 2){
-       Serial.println("세탁기1 사용 시작");
+       Serial.println("세탁기2 사용 시작");
       }
       
     }
     else if(state == 2){
       state = 1; //사용 종료
       if(state == 1){
-       Serial.println("세탁기1 사용 종료");
+       Serial.println("세탁기2 사용 종료");
       }
     }
   }
