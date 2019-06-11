ceaffaf549cee78e95073373cac8bb7fa66c0bed
park hoon <anrmrdowk@gmail.com>
2019-06-11 15:28:35 +0900

    re

--- washingmachine/washingmachine.ino
+++ washingmachine/washingmachine.ino
@@ -6,16 +6,16 @@
 #define DOUT  A1 //엠프 데이터 아웃 핀 넘버 선언
 #define CLK  A0  //엠프 클락 핀 넘버 
 HX711 scale(DOUT, CLK); //엠프 핀 선언 
-int buttonApin1 = 5; //세탁기1 버튼1
-int buttonBpin1 = 6; //세탁기1 버튼2
-int buttonCpin1 = 7; //세탁기1 버튼3
+int buttonApin = 5; //세탁기1 버튼1
+int buttonBpin = 6; //세탁기1 버튼2
+int buttonCpin = 7; //세탁기1 버튼3
 
-int state1 = 1; //세탁기1 상태 0..고장 1..사용가능 2..사용중
-int rg1 = 0; //세탁기1 예약 0..예약안됨 1..예약 중
+int state = 1; //세탁기1 상태 0..고장 1..사용가능 2..사용중
+int rg = 0; //세탁기1 예약 0..예약안됨 1..예약 중
 Timer t;
 int pin = 13; //타이머 설정 핀
-int lefttime1 = 1;  //세탁기1 남은빨래시간 가정
-int mode1 = 0; //세탁기 빨래 모드 0..저 1..중 2..고(가정)
+int lefttime = 1;  //세탁기1 남은빨래시간 가정
+int mode = 0; //세탁기 빨래 모드 0..저 1..중 2..고(가정)
 
 void setup() {
   Serial.begin(9600);  // 시리얼 통신 개방
@@ -23,9 +23,9 @@ void setup() {
   scale.set_scale(calibration_factor);  //스케일 지정 
   scale.tare();  //스케일 설정
   Serial.println("Readings:");
-  pinMode(buttonApin1, INPUT_PULLUP);
-  pinMode(buttonBpin1, INPUT_PULLUP);
-  pinMode(buttonCpin1, INPUT_PULLUP);
+  pinMode(buttonApin, INPUT_PULLUP);
+  pinMode(buttonBpin, INPUT_PULLUP);
+  pinMode(buttonCpin, INPUT_PULLUP);
   pinMode(pin,OUTPUT);
   t.pulse(pin,lefttime1*60*1000,HIGH);
 }
@@ -36,55 +36,59 @@ void loop() {
   Serial.print(" lbs"); //단위
   Serial.println(); 
   t.update();
-
-  if (digitalRead(buttonApin1) == LOW)
-  {
-    if(mode1 == 0){
+  if(state == 1){
+   if (digitalRead(buttonApin1) == LOW)
+    {
+      if(mode == 0){
       
-    }
-    else{
-      mode1--;//세탁모드 내림
-      if(mode1 == 0){
-        Serial.println("세탁모드 저");
-       }
-       if(mode1 == 1){
-        Serial.println("세탁모드 중");
-       }
-       if(mode1 == 2){
-        Serial.println("세탁모드 고");
-       }
-    }
-  }
-  if (digitalRead(buttonBpin1) == LOW)
-  {
-    if(mode1 == 2){
+      }
+     else{
+       mode--;//세탁모드 내림
+       if(mode == 0){
+          Serial.println("세탁모드 저");
+        }
+         if(mode == 1){
+          Serial.println("세탁모드 중");
+        }  
+        if(mode == 2){
+         Serial.println("세탁모드 고");
+        }
+      }
+   }
+   if (digitalRead(buttonBpin1) == LOW)
+    {
+     if(mode == 2){
       
-    }
-    else{
-      mode1++;//세탁모드 올림
-       if(mode1 == 0){
-        Serial.println("세탁모드 저");
-       }
-       if(mode1 == 1){
-        Serial.println("세탁모드 중");
-       }
-       if(mode1 == 2){
-        Serial.println("세탁모드 고");
-       }
-    }
+     }
+     else{
+        mode++;//세탁모드 올림
+         if(mode == 0){
+         Serial.println("세탁모드 저");
+         }
+         if(mode == 1){
+         Serial.println("세탁모드 중");
+         }  
+         if(mode == 2){
+          Serial.println("세탁모드 고");
+         }
+      }
+   }
   }
-  if (digitalRead(buttonCpin1) == LOW)
-  {
-    if(state1 == 1){
-      state1 = 2; //사용 시작
-      if(state1 == 2){
+    if (digitalRead(buttonCpin1) == LOW)
+   {
+     if(state == 0){
+        Serial.println("세탁기 고장");
+     }
+    if(state == 1){
+      state = 2; //사용 시작
+      if(state == 2){
        Serial.println("세탁기1 사용 시작");
       }
       
     }
-    else if(state1 == 2){
-      state1 = 1; //사용 종료
-      if(state1 == 1){
+    else if(state == 2){
+      state = 1; //사용 종료
+      if(state == 1){
        Serial.println("세탁기1 사용 종료");
       }
     }
